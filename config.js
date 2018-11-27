b8c3bd61817482c15a7a1cbadda31f13b1e3d019
Oskar Rosén <oskarrn93@gmail.com>
2018-11-27 20:36:34 +0100

    add config file with parameters

---
+++ config.js
@@ -0,0 +1,19 @@
+
+
+const nba = {};
+nba.teams = ["Celtics", "Lakers", "Warriors"];
+nba.url = "http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2018/league/00_full_schedule.json";
+
+nba.mongo = {}
+nba.mongo.url = "mongodb://localhost:27017/";
+nba.mongo.database = "events";
+nba.mongo.collection = "nba";
+
+const hltv = {};
+hltv.test = "hltv"
+
+
+module.exports = {
+   nba: nba,
+   hltv: hltv
+};
